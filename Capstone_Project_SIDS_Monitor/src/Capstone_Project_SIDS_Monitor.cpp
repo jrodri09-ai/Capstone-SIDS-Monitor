a8e32d25fbe6990f17e6eb2d284d738bec038715
Jessica Rodriquez <jrodriquez15@cnm.edu>
2021-12-03 11:06:22 -0700

    new test code implemented but SP02 still not working quite right

--- Capstone_Project_SIDS_Monitor/src/Capstone_Project_SIDS_Monitor.cpp
+++ Capstone_Project_SIDS_Monitor/src/Capstone_Project_SIDS_Monitor.cpp
@@ -25,16 +25,35 @@ SYSTEM_MODE(SEMI_AUTOMATIC)
 #include "math.h"
 #include "IoTTimer.h"
 #include "Stepper.h"
+#include <Wire.h>
+#include "MAX30105.h"
+#include "spo2_algorithm.h"
 
+#define OLED_RESET  D4 
+#define BUTTONPIN D2
+#define THREADPIN A5
+
+
+MAX30105 particleSensor;
+
+const int MAX_BRIGHTNESS=255;
 
 String DateTime, TimeOnly;
 
 IoTTimer Timer; 
 
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
 
-#define OLED_RESET  D4 
-#define BUTTONPIN D2
-#define THREADPIN A5
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+byte pulseLED = 11; //Must be on PWM pin
+byte readLED = D7; //Blinks with each data read
+
 
 int buttonpress;
 int resistance;
@@ -55,6 +74,8 @@ Stepper myStepper(stepsPerRevolution, A1, A3, A2, A4);
 
 void setup() {
   Serial.begin(9600);
+  Serial.begin(115200); // initialize serial communication at 115200 bits per second:
+
 // set the speed at 60 rpm:
   myStepper.setSpeed(12);
   
@@ -78,6 +99,42 @@ void setup() {
 
 pinMode(BUTTONPIN,INPUT);
 pinMode(THREADPIN,INPUT);
+pinMode(pulseLED, OUTPUT);
+pinMode(readLED, OUTPUT);
+
+ // Initialize sensor
+  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
+  {
+    Serial.printf("MAX30105 was not found. Please check wiring/power.");
+    while (1) delay(1000);
+  }
+
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+
+  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+
+  //read the first 100 samples, and determine the signal range
+  for (byte i = 0 ; i < bufferLength ; i++) {
+    while (particleSensor.available() == false) //do we have new data?
+      particleSensor.check(); //Check the sensor for new data
+
+    redBuffer[i] = particleSensor.getRed();
+    irBuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+    Serial.printf("red = %i, ir = %i \n",redBuffer[i],irBuffer[i]);
+  }
+
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
 }
 
 
@@ -137,7 +194,37 @@ Timer.startTimer(15000);
 
 Serial.println("clockwise");
   myStepper.step(-stepsPerRevolution);
+
+  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+  
+    delay(100); // allow the code to pause for a moment
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+    for (byte i = 25; i < 100; i++) {
+      redBuffer[i - 25] = redBuffer[i];
+      irBuffer[i - 25] = irBuffer[i];
+    }
+
+    //take 25 sets of samples before calculating the heart rate.
+    for (byte i = 75; i < 100; i++) {
+      while (particleSensor.available() == false) { //do we have new data?
+        particleSensor.check(); //Check the sensor for new data
+      }
+      digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
+
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+
+      //send samples and calculation result to terminal program through UART
+      if(((validHeartRate==1)||(validSPO2==1))&&(i==99)) {
+        Serial.printf("red = %i, ir = %i, HR = %i, HRvalid = %i, sp02 = %i, SPO2valid = %i \n",redBuffer[i],irBuffer[i],heartRate,validHeartRate,spo2,validSPO2);
+      }
+    }
+    //After gathering 25 new samples recalculate HR and SP02
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+
 }
+
 
 
 
